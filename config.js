596773142fe71b8498072d0e1f98a220364fcc08
Will Simmons <willsimmons@gmail.com>
2015-04-11 14:47:59 -0700

    test of react and streaming twitter

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  twitter: {
+    consumer_key: 'RhRQyKrFWPU20XgAb4F6IFUEj',
+    consumer_secret: 'qx1ySDEa6mniEB1zeAMabw2dKiuSFrcXscNvCdKa83NRcz2reC',
+    access_token_key: '303940540-0FdyaibQSZQkIVJcuRLq42jwGhxL7KmIuitFpESy',
+    access_token_secret: 'oQrOkpgWm49TwgDIRW2H4wGH1pqeRiuBpLId2iBATXLWA'
+  }
+}
